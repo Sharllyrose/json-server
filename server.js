03c36acf159443730e0bd4d22f15c30c29b48c69
Sharllyrose Wanjala <sharllyrose100@gmail.com>
2025-03-29 11:24:32 +0300

    Update server.js and add public directory

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('./db.json'); // Ensure the path to db.json is correct
+const middlewares = jsonServer.defaults({ static: './public' }); // Point to your public directory
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
